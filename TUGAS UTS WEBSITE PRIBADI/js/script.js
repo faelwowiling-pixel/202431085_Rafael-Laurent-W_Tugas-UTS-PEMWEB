c6ad61dc6a821760e5c8f028d91c89e071362e35
Rafael Laurent <faelwowiling3006@gmail.com>
2025-10-29 00:40:11 +0700

    Delete

--- TUGAS UTS WEBSITE PRIBADI/js/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle navigasi responsif
-document.querySelectorAll('[id^="nav-toggle"]').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const nav = btn.nextElementSibling;
-    nav.classList.toggle('active');
-  });
-});
